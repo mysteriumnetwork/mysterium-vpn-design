276377a8d1de330d6f66941f53de58c34fd85fe0
dmacan <david.macan@zeromolecule.com>
2025-12-16 11:25:49 +0100

    feat: refactor ModalHeader to include emblem and description

--- lib/widgets/modals/modal_header.dart
+++ lib/widgets/modals/modal_header.dart
@@ -1,47 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:mysterium_vpn_design/mysterium_vpn_design.dart';
 
-class ModalHeader extends StatelessWidget implements PreferredSizeWidget {
+class ModalHeader extends StatelessWidget {
   const ModalHeader({
-    this.title,
-    this.actions,
+    required this.emblem,
+    required this.title,
+    this.description,
+    this.emblemSpacing,
     super.key,
   });
 
-  final String? title;
-  final List<Widget>? actions;
+  final Widget emblem;
+  final double? emblemSpacing;
+
+  final String title;
+  final String? description;
 
   @override
   Widget build(BuildContext context) {
-    final parentRoute = ModalRoute.of(context);
     final theme = Theme.of(context);
-    final canPop = parentRoute?.impliesAppBarDismissal ?? false;
-    return AppBar(
-      elevation: 0,
-      backgroundColor: Palette.transparent,
-      forceMaterialTransparency: true,
-      automaticallyImplyLeading: false,
-      title: title == null ? null : Text(title!),
-      centerTitle: true,
-      actionsPadding: EdgeInsets.symmetric(horizontal: theme.spacing.xl2),
-      actions: [
-        ...?actions,
-        if (canPop) const _ModalCloseButton(),
+    final emblemSpacing = this.emblemSpacing ?? theme.spacing.xl3;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Align(
+          alignment: Alignment.topCenter,
+          child: emblem,
+        ),
+        SizedBox(height: emblemSpacing),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          maxLines: 2,
+          style: theme.textStyles.displayXlg.bold,
+        ),
+        if (description != null) ...[
+          SizedBox(height: theme.spacing.s),
+          Text(
+            description!,
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            style: theme.textStyles.textMd.regular.copyWith(color: theme.palette.textSecondary),
+          ),
+        ],
       ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(75);
-}
-
-class _ModalCloseButton extends StatelessWidget {
-  const _ModalCloseButton();
-
-  @override
-  Widget build(BuildContext context) => IconButton(
-        onPressed: Navigator.of(context).pop,
-        icon: const Icon(UntitledUI.x_close),
-        iconSize: 24,
-      );
 }
