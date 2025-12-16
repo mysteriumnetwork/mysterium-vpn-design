84c93ddeb98a4d3dd7ab86406f570ae1264d2080
dmacan <david.macan@zeromolecule.com>
2025-12-16 19:59:53 +0100

    feat: replace DecoratedBox with MaterialWrapper for improved tap handling in PlanContainer

--- lib/widgets/plan_card/plan_container.dart
+++ lib/widgets/plan_card/plan_container.dart
@@ -19,51 +19,57 @@ class _PlanContainer extends StatelessWidget {
     final bestValueBadge = data.bestValueBadge;
     final promoBadge = data.promoBadge;
 
-    return Stack(
-      fit: StackFit.passthrough,
-      children: [
-        if (bestValueBadge != null)
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: _PlanBestValueBanner(text: bestValueBadge),
-          ),
-        Padding(
-          padding: EdgeInsets.only(top: bestValueBadge != null ? 27 : 0),
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: theme.palette.bgPopover,
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(
-                color:
-                    isHighlighted ? theme.palette.borderBrandPrimary : theme.palette.borderPrimary,
-                strokeAlign: BorderSide.strokeAlignOutside,
-                width: isHighlighted ? 3 : 1,
+    return MaterialWrapper(
+      child: InkWell(
+        onTap: onTap,
+        child: Stack(
+          fit: StackFit.passthrough,
+          children: [
+            if (bestValueBadge != null)
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child: _PlanBestValueBanner(text: bestValueBadge),
+              ),
+            Padding(
+              padding: EdgeInsets.only(top: bestValueBadge != null ? 27 : 0),
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: theme.palette.bgPopover,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: isHighlighted
+                        ? theme.palette.borderBrandPrimary
+                        : theme.palette.borderPrimary,
+                    strokeAlign: BorderSide.strokeAlignOutside,
+                    width: isHighlighted ? 3 : 1,
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(theme.spacing.md),
+                  child: Stack(
+                    fit: StackFit.passthrough,
+                    children: [
+                      child,
+                      if (promoBadge != null)
+                        Positioned(
+                          right: 0,
+                          top: 0,
+                          child: Badge(
+                            text: promoBadge,
+                            size: BadgeSize.small,
+                            type: BadgeType.greenSecondary,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            child: Padding(
-              padding: EdgeInsets.all(theme.spacing.md),
-              child: Stack(
-                fit: StackFit.passthrough,
-                children: [
-                  child,
-                  if (promoBadge != null)
-                    Positioned(
-                      right: 0,
-                      top: 0,
-                      child: Badge(
-                        text: promoBadge,
-                        size: BadgeSize.small,
-                        type: BadgeType.greenSecondary,
-                      ),
-                    ),
-                ],
-              ),
-            ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
