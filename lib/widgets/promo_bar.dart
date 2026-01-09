dbfe9683f5cca56b10b5579dace3c5eccaeb67f6
dmacan <david.macan@zeromolecule.com>
2026-01-09 15:20:21 +0100

    feat: add PromoBar widget for customizable promotional actions

---
+++ lib/widgets/promo_bar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:mysterium_vpn_design/mysterium_vpn_design.dart';
+
+class PromoBar extends StatelessWidget {
+  const PromoBar({
+    required this.text,
+    this.onTap,
+    this.icon,
+    this.actionIcon = Icons.chevron_right,
+    super.key,
+  });
+
+  final Widget? icon;
+  final IconData actionIcon;
+  final String text;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return RawMaterialButton(
+      onPressed: onTap,
+      visualDensity: VisualDensity.compact,
+      elevation: 0,
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      fillColor: Palette.brand.shade300,
+      constraints: const BoxConstraints(minHeight: 40, maxHeight: 40),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48),
+            child: Row(
+              spacing: 8,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (icon != null)
+                  IconTheme(
+                    data: IconThemeData(color: Palette.brand.shade700),
+                    child: SizedBox.square(dimension: 24, child: icon),
+                  ),
+                Flexible(
+                  flex: 3,
+                  child: Text(
+                    text,
+                    style: theme.textStyles.textSm.semibold.copyWith(
+                      color: Palette.grayLight.shade800,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (onTap != null)
+            Positioned(
+              right: 0,
+              bottom: 0,
+              top: 0,
+              child: IconButton(
+                onPressed: onTap,
+                icon: Icon(actionIcon),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
